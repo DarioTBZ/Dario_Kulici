d563fc0ae7c8bdc34b6253689bfe00a637c5a44e
DarioTBZ <dario.kulici@edu.tbz.ch>
2024-11-05 16:34:56 +0100

    Obsidian Commit: 2024-11-05 16:34:56

--- 02_Projekte/Python/PythonNetworking/ServerClient/beta/serverv1.1.py
+++ 02_Projekte/Python/PythonNetworking/ServerClient/beta/serverv1.1.py
@@ -9,7 +9,6 @@ SYS_QUIT = "!quit"
 SYS_HELP = "!help"
 SYS_GAME = "!rps"
 
-# Decide if server should run on localhost or ip address in network
 def deployment():
     choose_deployment = ""
     while choose_deployment not in ["l", "n"]:
@@ -17,8 +16,8 @@ def deployment():
         choose_deployment = input()
     return choose_deployment
 
+# Deployment Auswahl ausführen
 choose_deployment = deployment()
-
 if choose_deployment == "l":
     SERVER = "127.0.0.1"
 elif choose_deployment == "n":
@@ -27,11 +26,8 @@ elif choose_deployment == "n":
 
 ADDR = (SERVER, PORT)
 
-# Create socket and set server to listen for incoming connections
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(ADDR)
-server.listen()
-server_running = True
+server_running = threading.Event()
+server_running.set()
 
 clients = []
 nicknames = []
@@ -39,13 +35,11 @@ nicknames = []
 def server_terminal():
     while True:
         command = input()
-        match command:
-            case "kill":
-                server_running = False
-                server.close()
-                return 1
-            case _:
-                pass
+        if command == "kill":
+            server_running.clear()  # Server beenden
+            server.close()
+            print("Server wird heruntergefahren...")
+            break
 
 def rps(client):
     client.send("rps".encode(FORMAT))
@@ -56,108 +50,90 @@ def broadcast(message, sender_client):
         if client != sender_client:
             client.send(message)
 
-
 def break_connection(client, addr):
-    index = clients.index(client)
-    clients.remove(client)
-    client.close()
-    nickname = nicknames[index]
-    broadcast(f"{nickname} left the chat.".encode(FORMAT), client)
-    print(f"Disconnected from {addr}, ({nickname}).")
-    nicknames.remove(nickname)
+    if client in clients:
+        index = clients.index(client)
+        clients.remove(client)
+        client.close()
+        nickname = nicknames[index]
+        broadcast(f"{nickname} left the chat.".encode(FORMAT), client)
+        print(f"Disconnected from {addr}, ({nickname}).")
+        nicknames.remove(nickname)
 
 def check_msg(msg, client):
     match msg.decode(FORMAT):
-        case "!quit": # replace manually if changing SYS constants
+        case "!quit":
             client.send(SYS_QUIT.encode(FORMAT))
             break_connection(client)
             return 1
-        case "!help": # replace manually if changing SYS constants
+        case "!help":
             client.send(f"""------------------------------------
-                        
 Available commands: 
-                        
     {SYS_QUIT} - Quits the server.
     {SYS_HELP} - Prints this menu
-                        
 Type in the chat to communicate with others. 
-        
-------------------------------------
-                    
-                        """.encode(FORMAT))
+------------------------------------""".encode(FORMAT))
         case "!rsp":
-            game_thread = threading.Thread(target=rps(client))    
+            game_thread = threading.Thread(target=rps, args=(client,))
             game_thread.start()
-
         case _:
             return 0
-    
 
 def handle_client(client, addr):
-    while True:
-        if server_running == False:
-            break
+    while server_running.is_set():
         try:
             message = client.recv(1024)
-            match check_msg(message, client):
-                case 1: # = client quits server
-                    break
-
+            if check_msg(message, client) == 1:
+                break
             broadcast(message, client)
         except:
             break_connection(client, addr)
             break
 
 def receive():
-    while True:
-        if server_running == False:
-            break
+    while server_running.is_set():
+        try:
+            client, addr = server.accept()
+            print(f"Connected with {str(addr)}.")
 
-        client, addr = server.accept()
-        print(f"Connected with {str(addr)}.")
+            client.send("NICK".encode(FORMAT))
+            nickname = client.recv(1024).decode(FORMAT)
+            nicknames.append(nickname)
+            clients.append(client)
 
-        # Get nickname from client
-        client.send("NICK".encode(FORMAT))
-        nickname = client.recv(1024).decode(FORMAT)
-        nicknames.append(nickname)
-        clients.append(client)
+            print(f"Nickname of the client is {nickname}")
+            broadcast(f"{nickname} joined the chat!".encode(FORMAT), client)
+            client.send("Connected to the server! Type !help to see a list of commands".encode(FORMAT))
 
-        print(f"Nickname of the client is {nickname}")
-        broadcast(f"{nickname} joined the chat!".encode(FORMAT), client)
-        client.send("Connected to the server! Type !help to see a list of commands".encode(FORMAT))
-
-        thread = threading.Thread(target=handle_client, args=(client, addr,))
-        thread.start()
-
-        
-
+            thread = threading.Thread(target=handle_client, args=(client, addr,))
+            thread.start()
+        except:
+            break  # Verlässt die Schleife, wenn `server.close()` ausgeführt wurde
 
 def process_timer():
-    while True:
-        if server_running == False:
-            break
-
+    while server_running.is_set():
         now = datetime.datetime.now()
         print(now.strftime("%Y.%m.%d, %H:%M:%S:"), " active threads: ", threading.active_count() - 1)
         time.sleep(60)
-        
 
-server_start = datetime.datetime.now()
+# Serverstart
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((SERVER, PORT))
+server.listen()
+
 print(f"""
     ------------------------------------
-    Server started at {server_start.strftime("%H:%M:%S")}:
-    
+    Server started:
     IP: {SERVER}
     Port: {PORT}
-      
     Server is online and listening...
     ------------------------------------
-      """)
+""")
 
 process_thread = threading.Thread(target=process_timer)
 process_thread.start()
 
-server_terminal = threading.Thread(target=server_terminal)
-server_terminal.start()
+server_terminal_thread = threading.Thread(target=server_terminal)
+server_terminal_thread.start()
 
-receive()
+receive()  # Beginne Verbindungen zu akzeptieren
